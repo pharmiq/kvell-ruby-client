309f2ebff80d02f199a923097342efa7b80f6a27
Akhmed <aha252000@mail.ru>
2025-04-28 19:42:03 +0300

    6596 - Проверка статуса возможности выплаты

--- spec/kvell/client_spec.rb
+++ spec/kvell/client_spec.rb
@@ -3,6 +3,17 @@
 RSpec.describe Kvell::Client do
   let(:client) { Kvell::Client.new }
 
+  let(:api_key) { 'api_key' }
+  let(:secret_key) { 'secret_key' }
+
+  before do
+    Kvell.configure do |config|
+      config.api_key = api_key
+      config.secret_key = secret_key
+      config.api_endpoint = 'https://api.pay.stage.kvell.group'
+    end
+  end
+
   describe '#fetch_banks' do
     subject(:result) do
       VCR.use_cassette("kvell/fetch_banks/#{cassette}", erb: erb) do
@@ -14,15 +25,6 @@ RSpec.describe Kvell::Client do
       {
         api_key: api_key,
       }
-    end
-
-    let(:api_key) { 'key' }
-
-    before do
-      Kvell.configure do |config|
-        config.api_key = api_key
-        config.api_endpoint = 'https://api.pay.stage.kvell.group'
-      end
     end
 
     context 'with valid data' do
@@ -80,17 +82,6 @@ RSpec.describe Kvell::Client do
       }
     end
 
-    let(:api_key) { 'api_key' }
-    let(:secret_key) { 'secret_key' }
-
-    before do
-      Kvell.configure do |config|
-        config.api_key = api_key
-        config.secret_key = secret_key
-        config.api_endpoint = 'https://api.pay.stage.kvell.group'
-      end
-    end
-
     let(:params) do
       Kvell::Requests::CheckPaymentPossibility.new do |cpp|
         cpp.amount = amount
@@ -136,6 +127,117 @@ RSpec.describe Kvell::Client do
           end.to raise_exception Faraday::UnprocessableEntityError,
                                  'the server responded with status 422'
         }
+      end
+    end
+  end
+
+  describe '#check_payment_possibility_status' do
+    subject(:result) do
+      VCR.use_cassette("kvell/check_payment_possibility_status/#{cassette}", erb: erb) do
+        client.check_payment_possibility_status(request_id)
+      end
+    end
+
+    let(:erb) do
+      {
+        api_key: api_key,
+        signature: Digest::SHA256.hexdigest("#{api_key}#{request_id}#{secret_key}"),
+      }
+    end
+
+    let(:request_id) { '57bc37bf-cf78-4393-98e1-67fa4fa801db' }
+
+    context 'with valid data' do
+      let(:cassette) { 'success' }
+
+      let(:data_expected) do
+        {
+          error_message: '',
+          fio_nspk: 'Иванов Иван Иванович',
+          nspk_id: 'B50731148152890M0000110011460901',
+          status: 'success',
+        }
+      end
+
+      it { expect(result).to have_attributes(data_expected) }
+    end
+
+    context 'with invalid data' do
+      context 'when api_key is nil' do
+        let(:cassette) { 'empty_api_key' }
+
+        let(:api_key) { nil }
+
+        it {
+          expect do
+            subject
+          end.to raise_exception Faraday::UnprocessableEntityError,
+                                 'the server responded with status 422'
+        }
+      end
+
+      context 'when api_key is invalid' do
+        let(:cassette) { 'invalid_api_key' }
+
+        let(:api_key) { 'invalid_api_key' }
+
+        it {
+          expect do
+            subject
+          end.to raise_exception Faraday::UnprocessableEntityError,
+                                 'the server responded with status 422'
+        }
+      end
+
+      context 'when amount is invalid' do
+        let(:cassette) { 'invalid_amount' }
+
+        let(:request_id) { '158bfba2-ba57-45f8-affb-db55d01cf5e8' }
+
+        let(:data_expected) do
+          {
+            error_message: 'Ошибка при разборе сообщения',
+            fio_nspk: '',
+            nspk_id: '',
+            status: 'error',
+          }
+        end
+
+        it { expect(result).to have_attributes(data_expected) }
+      end
+
+      context 'when fio is mismatched' do
+        let(:cassette) { 'mismatch_fio' }
+
+        let(:request_id) { 'a73abd5e-0882-4098-ba91-46a23813c7c4' }
+
+        let(:data_expected) do
+          {
+            error_message: 'Несовпадение ФИО получателя',
+            fio_nspk: 'Иванов Иван Иванович',
+            nspk_id: 'B5073115812064040000130011460901',
+            status: 'error',
+          }
+        end
+
+        it { expect(result).to have_attributes(data_expected) }
+      end
+
+      context 'when bank is not available' do
+        let(:cassette) { 'invalid_bank' }
+
+        let(:request_id) { '6ed9e6fb-7618-4b38-9121-8262c7d1c407' }
+
+        let(:data_expected) do
+          {
+            error_message: 'Ошибка логики в СБП: Не найден Получатель',
+            fio_nspk: '',
+            nspk_id: '',
+            status: 'error',
+          }
+        end
+
+        it { expect(result).to have_attributes(data_expected) }
       end
     end
   end
